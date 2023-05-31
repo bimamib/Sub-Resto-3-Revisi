538dbbaec1007d142c496342b95da9cdc44eb85b
bimamib <bimaprast1601@gmail.com>
2023-05-31 13:07:03 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
+import swRegister from './utils/sw-register';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+const START = 10;
+const NUMBER_OF_IMAGES = 100;
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
+
+const skipLink = document.querySelector('.skip-content');
+const mainContent = document.querySelector('#mainContent');
+
+skipLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  mainContent.scrollIntoView({ behavior: 'smooth' });
+  skipLink.blur();
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+});
